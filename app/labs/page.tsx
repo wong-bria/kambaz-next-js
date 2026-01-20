eba46245dfd3b17b4c49c956bc4f5d5051a88036
wong-bria <wong.bria@northeastern.edu>
2026-01-20 16:19:10 -0500

    test

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function labs() { 
  return ( 
    <div id="wd-labs"> 
-     <h1>Labs</h1> 
+     <h1>Labs Test</h1> 
      <ul> 
        <li> 
          <Link href="/labs/lab1" id="wd-lab1-link"> 
