18cfefe28e04c12b4b168977652581d85fff4e2a
wong-bria <wong.bria@northeastern.edu>
2026-02-09 22:42:50 -0500

    basics of javascript until section 3.5

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,6 +1,83 @@
+import VariablesAndConstants from "./variablesandconstants"; 
+import VariableTypes from "./variabletypes"
+import BooleanVariables from "./booleanvariables";
+import IfElse from "./ifelse";
+import TernaryOperator from "./ternaryoperator";
+import ConditionalOutputIfElse from "./conditionaloutputifelse";
+import ConditionalOutputInline from "./conditionaloutputinline";
+import LegacyFunctions from "./legacyfunctions";
+import ArrowFunctions from "./arrowfunctions";
+import ImpliedReturn from "./impliedreturn";
+import TemplateLiterals from "./templateliterals";
+import SimpleArrays from "./simplearrays";
+import ArrayIndexAndLength from "./arrayindexandlenght";
+import AddingAndRemovingToFromArrays from "./addingandremovingdatatofromarrays";
+import ForLoops from "./forloops";
+import MapFunction from "./mapfunction";
+import FindFunction from "./findfunction";
+import FindIndex from "./findindex";
+import FilterFunction from "./filterfunction";
+import JsonStringify from "./jsonstringify";
+import House from "./house";
+import Spreading from "./spreader";
+import Destructuring from "./destructing";
+import FunctionDestructing from "./functiondestructing";
+import DestructingImports from "./destructingimports";
+
 export default function Lab3() { 
-  return ( 
-    <div> 
-      <h2>Lab 3</h2> 
+  return( 
+    <div id="wd-lab3"> 
+      <h3>Lab 3</h3> 
+      <VariablesAndConstants/> 
+
+      <VariableTypes/>
+
+      <BooleanVariables/>
+
+      <IfElse/>
+
+      <TernaryOperator/>
+
+      <ConditionalOutputIfElse/>
+
+      <ConditionalOutputInline/>
+
+      <LegacyFunctions/>
+
+      <ArrowFunctions/>
+
+      <ImpliedReturn/>
+
+      <TemplateLiterals/>
+
+      <SimpleArrays/>
+
+      <ArrayIndexAndLength/>
+
+      <AddingAndRemovingToFromArrays/>
+
+      <ForLoops/>
+
+      <MapFunction/>
+
+      <FindFunction/>
+
+      <FindIndex/>
+
+      <FilterFunction/>
+
+      <JsonStringify/>
+
+      <House/>
+
+      <Spreading/>
+
+      <Destructuring/>
+
+      <FunctionDestructing/>
+
+      <DestructingImports/>
+      
+
     </div> 
-);}
+);} 
